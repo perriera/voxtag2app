b8dca593162f3a3bf6bfb5abe5d2f6daaff00410
Perry <perry@exparx.com>
2021-05-17 14:02:01 -0300

    Added LoadPath

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:voxtag2app/voxtag2/instance/VoxTag2.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(VoxTag2());
 }
