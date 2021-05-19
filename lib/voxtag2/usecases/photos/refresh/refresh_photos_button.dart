59f2b3bad683c136882f091a110d867c74a469cc
Perry <perry@exparx.com>
2021-05-19 15:13:19 -0300

    Added hook for PhotoAlbumManager

--- lib/voxtag2/usecases/photos/refresh/refresh_photos_button.dart
+++ lib/voxtag2/usecases/photos/refresh/refresh_photos_button.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:photo_album_manager/photo_album_manager.dart';
 import 'package:voxtag2app/voxtag2/extensions/NavBarButton.dart';
+import 'package:voxtag2app/voxtag2/system/load_path.dart';
 
 class RefreshPhotosButton extends StatelessWidget {
   const RefreshPhotosButton({
@@ -18,7 +20,16 @@ class RefreshPhotosButton extends StatelessWidget {
       icon: FontAwesomeIcons.redo,
       title: "Update",
       onTap: () {
-        print("RefreshPhotosButton");
+        LoadPath.init().then((applicationPath) {
+          print(applicationPath);
+          // TagsStorage.init(applicationPath);
+          // PhotosAlbum().init();
+        });
+        // PhotoAlbumManager.checkPermissions().then((status) {
+        //   print(status);
+        // }).catchError((error) {
+        //   print(error.toString());
+        // });
       }, // RefreshPhotoUseCase(context),
     );
     // });
