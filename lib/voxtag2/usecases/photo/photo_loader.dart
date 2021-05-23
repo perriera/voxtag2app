0d625895e6df81f51d1d57aa249927596a9573a4
Perry Anderson <perriera@users.noreply.github.com>
2021-05-23 10:07:08 -0300

    Added PhotoWidget

--- lib/voxtag2/usecases/photo/photo_loader.dart
+++ lib/voxtag2/usecases/photo/photo_loader.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:voxtag2app/voxtag2/usecases/photo/photo_widget.dart';
 import 'package:voxtag2app/voxtag2/usecases/photo/view_photo.dart';
 
 import 'photo_widget.dart' as pi;
@@ -25,19 +26,11 @@ class PhotoLoader extends StatelessWidget {
           Duration(seconds: 0),
           () async {
             try {
-              // var fi = FileImage(io.File(filename));
-              // PhotoLoader.fileImage = fi;
-              // previousFileName = filename;
-              // print(filename);
-              var img2 = Image.file(
+              return PhotoWidget(
+                  image: Image.file(
                 File(filename),
                 fit: BoxFit.contain,
-              );
-              return img2;
-              // return pi.PhotoWidget(
-              //   fileImage: fi,
-              //   photoId: PhotoViewer.voxTag,
-              // );
+              ));
             } catch (error) {
               var msg = 'PhotoLoader:future() $error';
               print(msg);
