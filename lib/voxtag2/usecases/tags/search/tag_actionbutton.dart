74c24b76555c51c7abdd3906025e8e73172d4992
Perry Anderson <perriera@users.noreply.github.com>
2021-05-24 19:48:56 -0300

    Added legacy code

---
+++ lib/voxtag2/usecases/tags/search/tag_actionbutton.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:voxtag2app/voxtag2/extensions/NavBarButton.dart';
+import 'package:voxtag2app/voxtag2/toast/toast_dialog.dart';
+
+import 'TagUpdateModeInterface.dart';
+import 'search_tags_actions.dart';
+
+class TagActionButton extends StatelessWidget {
+  const TagActionButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<TagUpdateModeInterface>(
+      initialData: SearchTagsAction(),
+      stream: tagActionMode.stream,
+      builder: (context, snapshot) {
+        return NavBarButton(
+          icon: snapshot.data.icon,
+          title: snapshot.data.title,
+          onTap: () {
+            var msg = 'Select a tag to ${snapshot.data.title}';
+            ToastDialog(context).display(msg, true);
+          },
+        );
+      },
+    );
+  }
+}
