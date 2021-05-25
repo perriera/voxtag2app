1f1809903aaf5c485aba76e33a67b3ef29e86ab7
Perry Anderson <perriera@users.noreply.github.com>
2021-05-25 13:50:07 -0300

    Added error message

--- lib/voxtag2/usecases/tags/obsolete/tags_grid.dart
+++ lib/voxtag2/usecases/tags/obsolete/tags_grid.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:voxtag2app/voxtag2/extensions/UBDError.dart';
+import 'package:voxtag2app/voxtag2/instance/Themes.dart';
 import 'package:voxtag2app/voxtag2/instance/VoxTag2.dart';
 import 'package:voxtag2app/voxtag2/usecases/photos/database/GridSizeInterface.dart';
 import 'package:voxtag2app/voxtag2/usecases/photos/database/GridSizeUpdate.dart';
 import 'package:voxtag2app/voxtag2/usecases/voxtags/VoxTags.dart';
 
 import 'tagged_widget.dart';
+import 'package:toast/toast.dart';
 
 class TagsGrid extends StatelessWidget {
   @override
@@ -16,14 +18,21 @@ class TagsGrid extends StatelessWidget {
         stream: gridSizeUpdate.stream,
         builder: (context, snapshot) {
           List<String> allTags = VoxTags().instance.allTags();
-          checkEmptySet(allTags);
-          return GridView.count(
-              primary: false,
-              padding: const EdgeInsets.all(20),
-              crossAxisSpacing: snapshot.data.size.toDouble(),
-              mainAxisSpacing: snapshot.data.size.toDouble(),
-              crossAxisCount: snapshot.data.size,
-              children: listTags(context, allTags));
+          try {
+            checkEmptySet(allTags);
+            return GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: snapshot.data.size.toDouble(),
+                mainAxisSpacing: snapshot.data.size.toDouble(),
+                crossAxisCount: snapshot.data.size,
+                children: listTags(context, allTags));
+          } catch (error) {
+            return Text(
+              error.toString(),
+              style: ThemeCatalog.kTextFieldStyle,
+            );
+          }
         },
       );
     } catch (e) {
