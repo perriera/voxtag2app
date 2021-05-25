abbb0ada768ddd8c995c923efd43fccd0b8e48db
Perry Anderson <perriera@users.noreply.github.com>
2021-05-25 20:07:38 -0300

    Added stuff

---
+++ lib/voxtag2/buttons/nav_bars/photos_navbar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:voxtag2app/voxtag2/buttons/more_options_button.dart';
+import 'package:voxtag2app/voxtag2/buttons/search_tags_button.dart';
+import 'package:voxtag2app/voxtag2/buttons/tag_photos_button.dart';
+import 'package:voxtag2app/voxtag2/instance/Themes.dart';
+import 'package:voxtag2app/voxtag2/buttons/refresh_photos_button.dart';
+
+class PhotosNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: ThemeCatalog.kNavBarColor,
+      height: 100.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          RefreshPhotosButton(),
+          TagPhotosButton(),
+          SearchTagsButton(),
+          MoreOptionsButton(),
+        ],
+      ),
+    );
+  }
+}
